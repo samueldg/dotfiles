27264f0fbdcf18917d5928a2720ce945006b9ae7
Samuel Dion-Girardeau <samuel.diongirardeau@gmail.com>
2022-12-06 17:22:09 -0500

    Fix bash multine variables

--- install-packages.sh
+++ install-packages.sh
@@ -2,44 +2,44 @@
 
 set -euo pipefail
 
-brew_formulae=(
-    asciinema
-    bat
-    black
-    chezmoi
-    curl
-    git
-    git-delta
-    git-lfs
-    gnu-sed
-    gnupg
-    kubernetes-cli
-    kustomize
-    pgcli
-    pinentry-mac
-    pipx
-    pre-commit
-    pyenv
-    pyenv-virtualenv
-    python-yq
-    ripgrep
-    tldr
-    tree
-    wget
-    xonsh
-)
+brew_formulae="\
+    asciinema \
+    bat \
+    black \
+    chezmoi \
+    curl \
+    git \
+    git-delta \
+    git-lfs \
+    gnu-sed \
+    gnupg \
+    kubernetes-cli \
+    kustomize \
+    pgcli \
+    pinentry-mac \
+    pipx \
+    pre-commit \
+    pyenv \
+    pyenv-virtualenv \
+    python-yq \
+    ripgrep \
+    tldr \
+    tree \
+    wget \
+    xonsh \
+"
 
-brew_casks=(
-    gpg-suite
-    keepassxc
-    keepingyouawake
-    ngrok
-)
+brew_casks="\
+    gpg-suite \
+    keepassxc \
+    keepingyouawake \
+    ngrok \
+"
 
-pipx_packages=(
-    flake8
-    poetry
-)
+pipx_packages="\
+    flake8 \
+    poetry \
+"
 
 brew install $brew_formulae
 brew install --cask $brew_casks
