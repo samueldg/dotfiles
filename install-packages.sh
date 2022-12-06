2a4c448f0712c285945416731d89122060e0236f
Samuel Dion-Girardeau <samuel.diongirardeau@gmail.com>
2022-12-06 12:41:54 -0500

    Add initial install script (tentative)

---
+++ install-packages.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -euo pipefail
+
+brew_formulae=(
+    asciinema
+    bat
+    black
+    chezmoi
+    curl
+    git
+    git-delta
+    git-lfs
+    gnu-sed
+    gnupg
+    kubernetes-cli
+    kustomize
+    pgcli
+    pinentry-mac
+    pipx
+    pre-commit
+    pyenv
+    pyenv-virtualenv
+    python-yq
+    ripgrep
+    tldr
+    tree
+    wget
+    xonsh
+)
+
+brew_casks=(
+    gpg-suite
+    keepassxc
+    keepingyouawake
+    ngrok
+)
+
+pipx_packages=(
+    flake8
+    poetry
+)
+
+brew install $brew_formulae
+brew install --cask $brew_casks
+pipx install $pipx_packages
